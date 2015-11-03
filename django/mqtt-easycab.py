c3885870908db25184f52e0f7411815c18e4a807
hanjo77 <hanjo77@gmail.com>
2015-11-04 00:21:31 +0100

    Removed unused code

--- django/mqtt-easycab.py
+++ django/mqtt-easycab.py
@@ -16,119 +16,51 @@ from data.models import Phone
 from data.models import Session
 from data.models import AppConfig
 
-def add_driver(token):
-    driver = Driver(
-        token=token
-    )
-    driver.save()
-    return driver.id
-
-def get_driver(token):
-    return Driver.objects.filter(token=token)[0].id
-
-def add_taxi(name):
-    taxi = Taxi(
-        name=name
-    )
-    taxi.save()
-    return taxi.id
-
-def get_taxi(name):
-    return Taxi.objects.filter(name=name)[0].id
-
-def add_phone(mac_addr):
-    phone = Phone(
-        mac=mac_addr
-    )
-    phone.save()
-    return phone.id
-
-def get_phone(mac_addr):
-    try:
-        return Phone.objects.filter(mac=mac_addr)[0].id
-    except:
-        return 0
-
-def add_session(driver_id, taxi_id, phone_id):
-    session = Session(
-        driver_id=driver_id,
-        taxi_id=taxi_id,
-        phone_id=phone_id,
-        start_time=datetime.datetime.now(),
-        end_time=datetime.datetime.now()
-    )
-    session.save()
-    return session.id
-
-def get_session(driver_id, taxi_id, phone_id):
-    timeout = datetime.datetime.now() - datetime.timedelta(minutes=1)
-    sessions = Session.objects.filter(end_time__gte=timeout).filter(driver_id=driver_id).filter(taxi_id=taxi_id).filter(phone_id=phone_id)
-    if sessions.exists():
-        session = sessions[0]
-        session.end_time = datetime.datetime.now()
-        session.save()
-        return session.id
-    else:
-        return 0
-
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print('Connected with result code '+str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe('session')
-    client.subscribe('presence')
-
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print('Connected with result code '+str(rc))
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe('session')
     client.subscribe('presence')
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     data = json.loads(msg.payload)
+    print data
     try:
-        if msg.topic == 'presence':
-            try:
-                session_id = data['session']
-                position = Position(
-                    session_id=session_id,
-                    latitude=data['gps']['latitude'],
-                    longitude=data['gps']['longitude'],
-                    time=datetime.datetime.now()
-                )
-                position.save()
-                session = Session.objects.filter(id=session_id).first()
-                session.end_time = datetime.datetime.now()
-                session.save()
-                driver = '';
-                if session.driver:
-                     driver = session.driver.token
-                message = json.dumps({
-                    'car': session.taxi.token,
-                    'driver': driver,
-                    'phone': session.phone.mac,
-                    'gps': {
-                        'latitude': position.latitude,
-                        'longitude': position.longitude
-                    },
-                    'time': str(position.time.replace(microsecond=0))
-                })
-                client.publish('position', message, qos=0, retain=True)
-                print message + " published to 'position'"
-            except Exception, e:
-                print str(e)
-    except AttributeError, e:
-        print(data)
-        print(str(e))
+        session_id = data['session']
+        position = Position(
+            session_id=session_id,
+            latitude=data['gps']['latitude'],
+            longitude=data['gps']['longitude'],
+            time=datetime.datetime.now()
+        )
+        position.save()
+        session = Session.objects.filter(id=session_id).first()
+        session.end_time = datetime.datetime.now()
+        session.save()
+        driver = '';
+        if session.driver:
+             driver = session.driver.token
+        message = json.dumps({
+            'car': session.taxi.token,
+            'driver': driver,
+            'phone': session.phone.mac,
+            'gps': {
+                'latitude': position.latitude,
+                'longitude': position.longitude
+            },
+            'time': str(position.time.replace(microsecond=0))
+        })
+        client.publish('position', message, qos=0, retain=True)
+        print message + " published to 'position'"
+    except Exception, e:
+        print str(e)
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-
+print "test"
 client.connect('46.101.17.239', 1883, 10)
 
 # Blocking call that processes network traffic, dispatches callbacks and
